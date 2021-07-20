9b236a92ccd53d8e15c43fb956f2d7d032568aed
Eduardo0507 <82070466+Eduardo0507@users.noreply.github.com>
2021-07-20 02:29:37 -0400

    Update api.py

--- api.py
+++ api.py
@@ -10,22 +10,22 @@ def read_root():
 
 @app.get("/sumar")
 def read_sumar(num1: int = 0, num2: int = 0):
-    return {calc.sumar(num1,num2)
+    return {"Resultado ":calc.sumar(num1,num2)
     }
 
 @app.get("/restar")
 def read_restar(num1: int = 0, num2: int = 0):
-    return {calc.restar(num1,num2)
+    return {"Resultado ":calc.restar(num1,num2)
     }
 
 @app.get("/multiplicar")
 def read_multiplicar(num1: int = 0, num2: int = 0):
-    return {calc.multiplicar(num1,num2)
+    return {"Resultado ":calc.multiplicar(num1,num2)
     }
 
 @app.get("/dividir")
 def read_dividir(num1: int = 0, num2: int = 0):
-    return {calc.dividir(num1,num2)
+    return {"Resultado ":calc.dividir(num1,num2)
     }
 
 
